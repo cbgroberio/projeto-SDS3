22488f765311c81a93321fb7d5c66faa90772b6c
Camilo Groberio <camilo.groberio@gmail.com>
2021-05-04 22:31:53 -0300

    estruturas do projeto criadas

---
+++ backend/src/main/java/com/devsuperior/desvendas/DesvendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/desvendas/DesvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
